867e8d4474df8f9ad9466da829674f1a4621bb85
CARLOS ALBERTO ZUCHERATO <464990725@unip-objetivo.br>
2020-02-28 09:38:44 -0300

    lançando eventos de domínio

--- src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using NerdStore.Core.Bus;
 using System.Threading.Tasks;
+using NerdStore.Catalogo.Domain.Events;
 
 namespace NerdStore.Catalogo.Domain
 {
     public class EstoqueService : IEstoqueService
     {
-        public EstoqueService(IProdutoRepository produtoRepository)
+        public EstoqueService(
+            IProdutoRepository produtoRepository,
+            IMediatrHandler bus)
         {
             _produtoRepository = produtoRepository;
+            _bus = bus;
         }
 
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IMediatrHandler _bus;
 
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
         {
@@ -20,6 +26,11 @@ namespace NerdStore.Catalogo.Domain
             if (!produto.PossuiEstoque(quantidade)) return false;
 
             produto.DebitarEstoque(quantidade);
+
+            if (produto.QuantidadeEstoque < 10)
+            {
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+            }
 
             _produtoRepository.Atualizar(produto);
             return await _produtoRepository.UnitOfWork.Commit();
---
+++ src/NerdStore.Core/Bus/MediatrHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MediatR;
+using System.Threading.Tasks;
+using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.Bus
+{
+    public class MediatrHandler : IMediatrHandler
+    {
+        public MediatrHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        private readonly IMediator _mediator;
+
+        public async Task PublicarEvento<T>(T evento) where T : Event
+        {
+            await _mediator.Publish(evento);
+        }
+    }
+}
