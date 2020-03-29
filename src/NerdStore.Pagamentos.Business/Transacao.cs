45d0830a04928752304ff9d87682d2a958d8fb4a
Carlos Alberto Zucherato <carlos.zucheratto@gmail.com>
2020-03-28 21:26:17 -0300

    Implementação camada de negócios do contexto de pagamentos

---
+++ src/NerdStore.Pagamentos.Business/Transacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Pagamentos.Business
+{
+    public class Transacao : Entity
+    {
+        public Guid PedidoId { get; set; }
+
+        public Guid PagamentoId { get; set; }
+
+        public decimal Total { get; set; }
+
+        public StatusTransacao StatusTransacao { get; set; }
+
+        public Pagamento Pagamento { get; set; }
+    }
+}
